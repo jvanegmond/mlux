f5d2ffbb299e9d48fefc1c7dd58ed7573c1f4db4
Jos van Egmond <info@josvanegmond.nl>
2017-02-02 23:03:16 +0100

    Add settings node to separate control

---
+++ Mlux.Wpf/SettingsGraph.xaml.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Mlux.Lib.Display;
+using Mlux.Lib.Time;
+using Mlux.Wpf.Bindings;
+
+namespace Mlux.Wpf
+{
+    public  enum NodeType
+    {
+        TimeOnly,
+        Brightness,
+        Temperature
+    }
+    /// <summary>
+    /// Interaction logic for SettingsGraph.xaml
+    /// </summary>
+    public partial class SettingsGraph : UserControl
+    {
+
+        private TimeProfileView _profile;
+        private Grid _draggingElement;
+        private NodeType _draggingType = NodeType.TimeOnly;
+
+        public const int AxisWidthMargin = 50;
+        public const int AxisHeightMargin = 30;
+
+        public TimeProfileView Profile
+        {
+            get { return _profile; }
+            set
+            {
+                _profile = value;
+            }
+        }
+
+        public SettingsGraph()
+        {
+            InitializeComponent();
+
+            SizeChanged += MluxSettingsGraph_SizeChanged;
+            Loaded += (sender, args) => MluxSettingsGraph_SizeChanged(sender, null);
+        }
+
+        private void DrawNodes()
+        {
+            GraphCanvas.Children.Clear();
+            GraphCanvas.Margin = new Thickness(AxisWidthMargin, 0, 0, AxisHeightMargin);
+
+            // Draw the nodes
+            foreach (var node in _profile.Nodes)
+            {
+                GraphCanvas.Children.Add(new SettingsGraphNode() { Node = node });
+            }
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            _draggingElement = null;
+            _draggingType = NodeType.TimeOnly;
+            Mouse.OverrideCursor = null;
+
+            base.OnMouseLeftButtonUp(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (_draggingElement == null) return;
+
+            var pos = e.GetPosition(GraphCanvas);
+
+            var nodeIndex = Array.IndexOf(GraphCanvas.Children.OfType<Grid>().ToArray(), _draggingElement);
+            var node = _profile.Nodes[nodeIndex];
+
+            var minTime = TimeSpan.Zero;
+            if (nodeIndex > 0)
+            {
+                var prevNode = _profile.Nodes[nodeIndex - 1];
+                minTime = prevNode.TimeOfDay.Add(TimeSpan.FromMinutes(15));
+            }
+
+            var width = Math.Max(100, GraphCanvas.ActualWidth);
+            var height = Math.Max(100, GraphCanvas.ActualHeight);
+
+            // Get time stuff (horizontal)
+            var xRel = (pos.X - AxisWidthMargin) / (width - AxisWidthMargin);
+            xRel = Math.Min(1d, xRel);
+            var roundedDays = Math.Round(xRel * 24 * 4) / 24 / 4;
+            var newTimeSpan = TimeSpan.FromDays(roundedDays);
+            if (newTimeSpan < minTime) // TODO: Same thing with max time
+            {
+                newTimeSpan = minTime;
+            }
+            node.TimeOfDay = newTimeSpan;
+
+            var percentageWidth = node.TimeOfDay.TotalSeconds / TimeSpan.FromDays(1).TotalSeconds;
+            var x = AxisWidthMargin + percentageWidth * (width - AxisWidthMargin);
+
+            _draggingElement.Margin = new Thickness(x, 0, 0, 0);
+
+            if (_draggingType != NodeType.TimeOnly)
+            {
+                // Get value of thing
+                var yRel = 1d - (pos.Y / height);
+                SetNodeValuePercentage(node, yRel, _draggingType);
+
+                var value = GetCurrentValue(node, _draggingType);
+                var percentageHeight = 1d - GetNodeValuePercentage(value, _draggingType);
+
+                var y = percentageHeight * (height - AxisHeightMargin);
+
+                var valueGrid = _draggingElement.Children.OfType<Grid>().ElementAt(((int)_draggingType - 1));    // based on index of grid in children
+                valueGrid.Margin = new Thickness(0, y, 0, 0);
+            }
+            base.OnMouseMove(e);
+        }
+
+        private void SetNodeValuePercentage(TimeNodeView node, double percentage, NodeType type)
+        {
+            if (type == NodeType.Brightness)
+            {
+                // Round it to 5
+                node.Brightness = RoundTo((int)(percentage * 100d), 5);
+            }
+            else
+            {
+                node.Temperature = RoundTo((int)((percentage * 3200d) + 3300d), 50);
+            }
+        }
+
+        private int RoundTo(int value, int roundTo)
+        {
+            var remainder = value % roundTo;
+            if (remainder <= (roundTo / 2))
+            {
+                return value - remainder; // round down
+            }
+            else
+            {
+                return value - remainder + roundTo; // round up
+            }
+        }
+
+        private void MluxSettingsGraph_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawChrome();
+            DrawNodes();
+        }
+
+        private void DrawChrome()
+        {
+            var width = Math.Max(100, ChromeCanvas.ActualWidth);
+            var height = Math.Max(100, ChromeCanvas.ActualHeight);
+
+            ChromeCanvas.Children.Clear();
+
+            // Draw hour of day on horizontal axis/scale
+            for (var n = 0; n <= 24; n++)
+            {
+                var percentageWidth = n / 24d;
+
+                ChromeCanvas.Children.Add(new Label()
+                {
+                    Margin = new Thickness(AxisWidthMargin + (percentageWidth * (width - AxisWidthMargin)), height - AxisHeightMargin, 0, 0),
+                    Height = AxisHeightMargin,
+                    Content = $"{n}:00"
+                });
+            }
+            ChromeCanvas.Children.Add(new Rectangle()
+            {
+                Fill = new SolidColorBrush(Colors.Black),
+                Margin = new Thickness(AxisWidthMargin, height - AxisHeightMargin, 0, 0),
+                Height = 1,
+                Width = width - AxisWidthMargin
+            });
+
+            // Draw brightness on vertical axis/scale
+            for (var n = 0; n <= 100; n += 10)
+            {
+                var percentageHeight = 1d - GetNodeValuePercentage(n, NodeType.Brightness);
+
+                ChromeCanvas.Children.Add(new Label()
+                {
+                    Margin = new Thickness(0, percentageHeight * (height - AxisHeightMargin /* - label height ?? */), 0, 0),
+                    Content = $"{n}"
+                });
+            }
+            ChromeCanvas.Children.Add(new Rectangle()
+            {
+                Fill = new SolidColorBrush(Colors.Black),
+                Margin = new Thickness(AxisWidthMargin, 0, 0, 0),
+                Height = height - AxisHeightMargin,
+                Width = 1
+            });
+
+            // Draw the current position
+            var now = TimeProvider.Now;
+            var w = now.TimeOfDay.TotalSeconds / TimeSpan.FromDays(1).TotalSeconds;
+            double x = AxisWidthMargin + (w * (width - AxisWidthMargin));
+
+            ChromeCanvas.Children.Add(new Rectangle()
+            {
+                Fill = new SolidColorBrush(Colors.DarkRed),
+                Margin = new Thickness(x, 0, 0, 0),
+                Height = height - AxisHeightMargin,
+                Width = 2
+            });
+        }
+
+        private Color GetBackgroundColor(int temperature, int brightness)
+        {
+            // get color gamma adjustment
+            var gamma = ColorTemperature.GetColorProfile(temperature);
+
+            // turn brightness 0-100 into a value between 0.6-1.0 (first between 0-0.4 then add 0.6)
+            var brightnessAdjust = (brightness / 250d) + 0.6d;
+
+            return new Color()
+            {
+                A = byte.MaxValue,
+                R = (byte)(byte.MaxValue * brightnessAdjust * gamma.Red),
+                G = (byte)(byte.MaxValue * brightnessAdjust * gamma.Green),
+                B = (byte)(byte.MaxValue * brightnessAdjust * gamma.Blue),
+            };
+        }
+
+        private void Delete_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNew_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public static double GetNodeValuePercentage(int value, NodeType type)
+        {
+            if (type == NodeType.Brightness)
+            {
+                return (value - TimeProfile.MinBrightness) / (double)(TimeProfile.MaxBrightness - TimeProfile.MinBrightness);
+            }
+            else
+            {
+                return ((double)value - (TimeProfile.MaxTemperature - TimeProfile.MinTemperature)) / TimeProfile.MinTemperature;
+            }
+        }
+
+        public static int GetCurrentValue(TimeNodeView node, NodeType type)
+        {
+            if (type == NodeType.Brightness)
+            {
+                return node.Brightness;
+            }
+            else
+            {
+                return node.Temperature;
+            }
+        }
+    }
+}
---
+++ Mlux.Wpf/SettingsGraphNode.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Mlux.Wpf.Bindings;
+
+namespace Mlux.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SetttingsGraphNode.xaml
+    /// </summary>
+    public partial class SettingsGraphNode : UserControl
+    {
+        private NodeType _draggingType = NodeType.TimeOnly;
+        private TimeNodeView _node;
+
+        public event EventHandler StartDrag;
+        public event EventHandler MouseOver;
+
+        public SettingsGraphNode()
+        {
+            InitializeComponent();
+            Cursor = Cursors.SizeWE;
+
+            Loaded += (sender, args) => Redraw();
+        }
+
+        public TimeNodeView Node
+        {
+            get { return _node; }
+            set
+            {
+                _node = value;
+                Redraw();
+            }
+        }
+
+        private void Redraw()
+        {
+            if (Parent == null) return;
+
+            var width = ((Panel)Parent).ActualWidth;
+            var height = ((Panel)Parent).ActualHeight;
+
+            Height = height;
+
+            var percentageWidth = _node.TimeOfDay.TotalSeconds / TimeSpan.FromDays(1).TotalSeconds;
+            var x =  percentageWidth * width;
+
+            Margin = new Thickness(x, 0, 0, 0);
+        }
+
+        private void TemperatureNode_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _draggingType = NodeType.Temperature;
+        }
+
+        private void BrightnessNode_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _draggingType = NodeType.Brightness;
+        }
+
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var width = ((Panel)Parent).ActualWidth;
+            var height = ((Panel)Parent).ActualHeight;
+
+            // Start drag
+
+            var percentageWidth = Node.TimeOfDay.TotalSeconds / TimeSpan.FromDays(1).TotalSeconds;
+            var x = percentageWidth * width;
+
+            var draggableElement = new Grid
+            {
+                Margin = new Thickness(x, 0, 0, 0),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Cursor = Cursors.SizeWE,
+            };
+            draggableElement.MouseLeftButtonDown += DraggableOnMouseLeftButtonDown;
+            draggableElement.MouseEnter += DraggableMouseEnter;
+
+            // Draw vertical line
+            draggableElement.Children.Add(new Rectangle()
+            {
+                Fill = new SolidColorBrush(Colors.Blue),
+                Height = height,
+                Width = 20
+            });
+
+            // Draw draggable brightness node
+            var brightnessY = (1 - SettingsGraph.GetNodeValuePercentage(SettingsGraph.GetCurrentValue(Node, NodeType.Brightness), NodeType.Brightness)) * height;
+            var brightnessGrid = new Grid()
+            {
+                Children =
+                {
+                    new Ellipse()
+                    {
+                        Fill = new SolidColorBrush(Colors.Gray),
+                        Height = 15,
+                        Width = 15,
+                    }
+                },
+                Margin = new Thickness(0, brightnessY, 0, 0),
+                VerticalAlignment = VerticalAlignment.Top
+            };
+            brightnessGrid.MouseLeftButtonDown += BrightnessGrid_MouseLeftButtonDown;
+            draggableElement.Children.Add(brightnessGrid);
+
+            // Draw draggable temperature node
+            var temperatureY = (1 - SettingsGraph.GetNodeValuePercentage(SettingsGraph.GetCurrentValue(Node, NodeType.Temperature), NodeType.Temperature)) * height;
+            var temperatureGrid = new Grid()
+            {
+                Children =
+                {
+                    new Ellipse()
+                    {
+                        Fill = new SolidColorBrush(Colors.Red),
+                        Height = 15,
+                        Width = 15,
+                    }
+                },
+                Margin = new Thickness(0, temperatureY, 0, 0),
+                VerticalAlignment = VerticalAlignment.Top
+            };
+            temperatureGrid.MouseLeftButtonDown += TemperatureGrid_MouseLeftButtonDown;
+            draggableElement.Children.Add(temperatureGrid);
+
+            StartDrag?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void TemperatureGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _draggingType = NodeType.Temperature;
+        }
+
+        private void BrightnessGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _draggingType = NodeType.Brightness;
+        }
+
+        private void DraggableOnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.SizeWE;
+        }
+
+        private void DraggableMouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set the top bar values to the currently mouse-over node
+            MouseOver?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
